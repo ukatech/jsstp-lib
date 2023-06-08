16ac0382ce16542cc13159119314d194162ea948
steve02081504 <steve02081504@EDEN.fukwold>
2023-06-08 16:21:07 +0800

    Update jsstp.js

--- jsstp.js
+++ jsstp.js
@@ -20,16 +20,15 @@ class sstp_info_t {
 	constructor(info_head, info_body, unknown_lines) {
 		this.set_head(info_head);
 		this.#unknown_lines = unknown_lines;
-		if(info_body){
-			if(info_body.keys)
-				for(let key of info_body.keys())
+		if (info_body) {
+			if (info_body.keys)
+				for (let key of info_body.keys())
 					this[key] = info_body.get(key);
-			else if(typeof(info_body) == "object")
-				for(let key in info_body)
-					this[key] = info_body[key];	
+			else if (typeof info_body == "object")
+				for (let key in info_body)
+					this[key] = info_body[key];
 			//否则记录错误
-			else
-				console.error("sstp_info_t: info_body is not a Map or object: " + typeof(info_body));
+			else console.error("sstp_info_t: info_body is not a Map or object: " + typeof info_body);
 		}
 	}
 	//自字符串报文构造
@@ -41,9 +40,8 @@ class sstp_info_t {
 		let unknown_lines = [];
 		let spliter_list = [": ", String.fromCharCode(1)];
 		for (let i = 0; i < body.length; i++) {
-			let line=body[i];
-			if(line == "")
-				continue;
+			let line = body[i];
+			if (line == "") continue;
 			let spliter = "";
 			for (let j = 0; j < spliter_list.length; j++)
 				if (line.indexOf(spliter_list[j]) != -1) {
@@ -55,105 +53,92 @@ class sstp_info_t {
 				let value = line.replace(key + spliter, "");
 				thebody.set(key, value);
 			}
-			else{
+			else
 				unknown_lines.push(body[i]);
-			}
 		}
 		return new sstp_info_t(thehead, thebody, unknown_lines);
 	}
 	//设置报文头
 	set_head(head) {
-		if(head == undefined)
+		if (!head)
 			head = "NOTIFY SSTP/1.1";
 		this.#head = head;
 	}
 	//获取报文体
 	get_body() {
 		let body = new Map();
-		for(let key in this)
+		for (let key in this)
 			body.set(key, this[key]);
 		return body;
 	}
 	//获取未知行
-	get_unknown_lines() {
-		return this.#unknown_lines;
-	}
+	get_unknown_lines() { return this.#unknown_lines; }
 	//获取报文头
-	get_head() {
-		return this.#head;
-	}
+	get_head() { return this.#head; }
 	//获取报文
 	to_string() {
 		let str = this.#head + "\r\n";
 		for (let key in this)
 			str += `${key}: ${this[key]}\r\n`;
-		str += "\r\n";
-		return str;
+		return str + "\r\n";
 	}
 	//获取报头返回码
 	return_code() {
 		//比如：SSTP/1.4 200 OK，返回200
 		let code_table = this.#head.split(" ");
-		for(let i = 0; i < code_table.length; i++)
-			if(!isNaN(code_table[i]))
+		for (let i = 0; i < code_table.length; i++)
+			if (!isNaN(code_table[i]))
 				return parseInt(code_table[i]);
 		return -1;
 	}
-	get_passthrough(key) {
-		const passthrough = "X-SSTP-PassThru-" + key;
-		return this[passthrough];
-	}
-};
-class sstp_fmo_info_t{
+	get_passthrough(key) { return this["X-SSTP-PassThru-" + key]; }
+}
+
+class sstp_fmo_info_t {
 	//构造函数
-	constructor(fmo_info={}) {
+	constructor(fmo_info = {}) {
 		//fmo_info每个key的格式都是"uuid.属性名"
-		for(let key in fmo_info){
+		for (let key in fmo_info) {
 			let uuid = key.split(".")[0];
 			let name = key.split(".")[1];
-			if(!this[uuid])
-				this[uuid] = {};
+			if (!this[uuid]) this[uuid] = {};
 			this[uuid][name] = fmo_info[key];
 		}
 	}
 	get_uuid_by(name, value) {
-		for(let uuid in this)
-			if(this[uuid][name] == value)
+		for (let uuid in this)
+			if (this[uuid][name] == value)
 				return uuid;
 		return null;
 	}
 	get_list_of(name) {
 		let list = [];
-		for(let uuid in this)
+		for (let uuid in this)
 			list.push(this[uuid][name]);
 		return list;
 	}
-	keys() {
-		return Object.keys(this);
-	}
-	length() {
-		return this.keys().length;
-	}
-	available() {
-		return this.length() != 0;
-	}
-};
-class ghost_events_queryer_t{
+	keys() { return Object.keys(this); }
+	length() { return this.keys().length; }
+	available() { return this.length() != 0; }
+}
+
+class ghost_events_queryer_t {
 	#base_jsstp;
 	#ghost_has_has_event;
 	#ghost_has_get_supported_events;
 	#ghost_event_list;
 	#ghost_event_list_cache;
 
-	constructor(base_jsstp=jsstp) {
+	constructor(base_jsstp = jsstp) {
 		this.#base_jsstp = base_jsstp;
 	}
-	async check_event(event_name, security_level="local") {
-		if(this.#ghost_has_get_supported_events)
+	async check_event(event_name, security_level = "local") {
+		if (this.#ghost_has_get_supported_events)
 			return this.#ghost_event_list[security_level].includes(event_name);
-		else if(this.#ghost_has_has_event){
-			if(this.#ghost_event_list_cache[security_level][event_name] != undefined)
-				return this.#ghost_event_list_cache[security_level][event_name];
+		else if (this.#ghost_has_has_event) {
+			let charge = this.#ghost_event_list_cache[security_level][event_name];
+			if (charge != undefined)
+				return charge;
 			let result = await this.#base_jsstp.has_event(event_name);
 			this.#ghost_event_list_cache[security_level][event_name] = result;
 			return result;
@@ -161,54 +146,49 @@ class ghost_events_queryer_t{
 		else
 			return false;
 	}
-	available(){
-		return this.#ghost_has_has_event;
-	}
-	get_supported_events_available(){
-		return this.#ghost_has_get_supported_events;
-	}
+	available() { return this.#ghost_has_has_event; }
+	get_supported_events_available() { return this.#ghost_has_get_supported_events; }
 	async reset() {
 		this.clear();
 		this.#ghost_has_has_event = await this.#base_jsstp.has_event("Has_Event");
 		this.#ghost_has_get_supported_events = await this.#base_jsstp.has_event("Get_Supported_Events");
-		if(this.#ghost_has_get_supported_events)
+		if (this.#ghost_has_get_supported_events)
 			this.#ghost_event_list = await this.#base_jsstp.get_supported_events();
 	}
-	async init() {
-		await this.reset();
-	}
+	async init() { await this.reset(); }
 	clear() {
 		this.#ghost_has_has_event = false;
 		this.#ghost_has_get_supported_events = false;
 		this.#ghost_event_list = null;
 		this.#ghost_event_list_cache = {
-			"local": {},
-			"external": {}
+			local: {},
+			external: {}
 		};
 	}
-};
+}
+
 //定义一个包装器
 class jsstp_t {
 	#headers;
 	#default_info;
 	#host;
 
-	constructor(sendername,host) {
+	constructor(sendername, host) {
 		this.#headers = new Map();
 		//初始化默认的host
 		this.set_host(host);
 		this.set_RequestHeader("Content-Type", "text/plain");
 		//如果可以的话获取url并设置Origin
-		if(window.location.origin)
+		if (window.location.origin)
 			this.set_RequestHeader("Origin", window.location.origin);
 		//初始化默认的报文
 		this.#default_info = new Map();
-		this.set_default_info("Charset","UTF-8");
+		this.set_default_info("Charset", "UTF-8");
 		this.set_sendername(sendername);
 	}
 	//set_RequestHeader
 	set_RequestHeader(key, value) {
-		if(value == null)
+		if (value == null)
 			delete this.#headers[key];
 		else
 			this.#headers[key] = value;
@@ -218,62 +198,58 @@ class jsstp_t {
 		this.#default_info = info;
 	}
 	set_default_info(key, value) {
-		if(value == null)
+		if (value == null)
 			delete this.#default_info[key];
 		else
 			this.#default_info[key] = value;
 	}
 	//修改host
 	set_host(host) {
-		if(host == undefined)
+		if (!host)
 			host = "http://localhost:9801/api/sstp/v1";
 		this.#host = host;
 	}
 	//修改sendername
 	set_sendername(sendername) {
-		if(sendername == undefined)
+		if (!sendername)
 			sendername = "jsstp-client";
 		this.#default_info["Sender"] = sendername;
 	}
 	#base_post(data, callback) {
 		//使用fetch发送数据
 		const param = {
-			method: 'POST',
+			method: "POST",
 			headers: this.#headers,
 			body: data
 		};
-		let call_base=(resolve, reject) => {
-			fetch(this.#host, param).then(function(response) {
-				if(response.status != 200)
+		let call_base = (resolve, reject) => {
+			fetch(this.#host, param).then(function (response) {
+				if (response.status != 200)
 					reject(response.status);
-				else
-					response.text().then(function(text) {
-						resolve(sstp_info_t.from_string(text));
-					});
+				else response.text().then(function (text) {
+					resolve(sstp_info_t.from_string(text));
+				});
 			});
-		}
-		if(callback)
-			call_base(callback, function(){});
+		};
+		if (callback)
+			call_base(callback, function () { });
 		//如果callback不存在，返回一个promise
-		if(callback == undefined)
+		if (callback == undefined)
 			return new Promise(call_base);
 	}
 	//发送报文
 	costom_send(sstphead, info, callback) {
-		if (typeof (info) == "object") {
+		if (typeof info == "object") {
 			//获取报文
 			let data = new sstp_info_t();
 			data.set_head(sstphead);
-			for (let key in this.#default_info)
-				data[key]=this.#default_info[key];
-			for (let key in info)
-				data[key]= info[key];
+			for (let key in this.#default_info) data[key] = this.#default_info[key];
+			for (let key in info) data[key] = info[key];
 			//使用base_post发送
 			return this.#base_post(data.to_string(), callback);
 		}
 		//否则记录错误
-		else
-			console.error("jsstp.send: wrong type of info: " + typeof(info));
+		else console.error("jsstp.send: wrong type of info: " + typeof info);
 	}
 	//发送报文
 	//SEND SSTP/1.4
@@ -298,7 +274,7 @@ class jsstp_t {
 	}
 	//根据type发送报文
 	by_type(type) {
-		return eval("this."+type).bind(this);
+		return eval("this." + type).bind(this);
 	}
 	//has_event
 	/*
@@ -324,9 +300,9 @@ class jsstp_t {
 	*/
 	async has_event(event_name, security_level = "external") {
 		const info = await this.SEND({
-			"Event": "Has_Event",
-			"Reference0": event_name,
-			"Reference1": security_level
+			Event: "Has_Event",
+			Reference0: event_name,
+			Reference1: security_level
 		});
 		const result = info.get_passthrough("Result");
 		return !!result && result != "0";
@@ -369,7 +345,7 @@ class jsstp_t {
 	*/
 	async get_supported_events() {
 		const info = await this.SEND({
-			"Event": "Get_Supported_Events"
+			Event: "Get_Supported_Events"
 		});
 		const local = info.get_passthrough("local");
 		const external = info.get_passthrough("external");
@@ -379,13 +355,12 @@ class jsstp_t {
 		};
 	}
 	async get_fmo_infos() {
-		let fmo={};
-		try{
+		let fmo = {};
+		try {
 			fmo = await this.EXECUTE({
-				"Command": "GetFMO"
+				Command: "GetFMO"
 			});
-		}
-		catch(e){}
+		} catch (e) { }
 		return new sstp_fmo_info_t(fmo);
 	}
 	async available() {
@@ -396,7 +371,7 @@ class jsstp_t {
 		await result.init();
 		return result;
 	}
-};
+}
 
 var jsstp = new jsstp_t();
 
