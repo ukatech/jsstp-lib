3dd2326b8b0095b8b153c6d9ff87b8c3600cffee
Taromati2 <taromati2@outlook.com>
2023-07-08 14:27:52 +0000

    file update~

--- dist/jsstp.d.ts
+++ dist/jsstp.d.ts
@@ -656,6 +656,39 @@ declare class jsstp_t{
 }
 
 /**
+ * 一个可用函数初始化的可扩展的函数类型，用于更为可读的派生类函数类型
+ */
+declare class ExtensibleFunction<args_T extends Array<any>,return_T> extends Function {
+	/**
+	 * 自函数实例初始化
+	 * @param {Function} func
+	 * @returns {ExtensibleFunction}
+	 */
+	constructor(func: (...args: args_T) => return_T);
+	/**
+     * 调用函数，用指定的对象代替函数的this值，用指定的数组代替函数的参数。
+     * @param thisArg 将被用作this对象的对象。
+     * @param argArray 一组要传递给函数的参数。
+     */
+    apply(thisArg: (...args: args_T) => return_T, argArray?: args_T): return_T;
+
+    /**
+     * 调用一个对象的方法，用另一个对象代替当前对象。
+     * @param thisArg 将被用作当前对象的对象。
+     * @param argArray 要传递给方法的参数列表。
+     */
+    call(thisArg: (...args: args_T) => return_T, ...argArray: args_T): return_T;
+
+    /**
+     * 对于一个给定的函数，创建一个绑定的函数，其主体与原函数相同。
+     * 绑定函数的this对象与指定的对象相关联，并具有指定的初始参数。
+     * @param thisArg 一个对象，this关键字可以在新函数中引用。
+     * @param argArray 一个要传递给新函数的参数列表。
+     */
+    bind(thisArg: (...args: args_T) => return_T, ...argArray: any): (...args: args_T) => return_T;
+}
+
+/**
  * ghost事件查询器
  * @example
  * let ghost_events_queryer = jsstp.new_event_queryer();
@@ -666,7 +699,7 @@ declare class jsstp_t{
  * @alias jsstp.ghost_events_queryer_t
  * @see {@link jsstp_t.new_event_queryer}
  */
-declare class ghost_events_queryer_t {
+declare class ghost_events_queryer_t extends ExtensibleFunction<string[],Promise<Boolean>> {
 	/**
 	 * 构造一个事件查询器
 	 * @param {jsstp_t} base_jsstp
@@ -683,6 +716,17 @@ declare class ghost_events_queryer_t {
 	 * @see 基于 {@link jsstp_t.has_event} 和 {@link jsstp_t.get_supported_events}
 	 */
 	/*@__PURE__*/check_event(event_name: String, security_level?: String): Promise<Boolean>;
+	/**
+	 * 调用声明
+	 * 检查事件是否存在，ghost至少需要`Has_Event`事件的支持，并可以通过提供`Get_Supported_Events`事件来提高效率
+	 * @param {String} event_name
+	 * @param {String|undefined} security_level
+	 * @returns {Promise<Boolean>}
+	 * @example
+	 * let result = await ghost_events_queryer("On_connect");
+	 * @see 基于 {@link ghost_events_queryer_t.check_event}
+	 */
+	/*@__PURE__*///**remove from dist**//(event_name: String, security_level?: String): Promise<Boolean>;
 	/**
 	 * 检查是否能够检查事件
 	 * @returns {Promise<Boolean>}
